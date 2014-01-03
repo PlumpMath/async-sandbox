a1da84621cca314e1d7d89a8161180b63d5a3b6c
Sergei Barbarash <sgt.sgt@gmail.com>
2014-01-03 19:54:27 +0700

    better sub syntax

--- src/async_sandbox/pubsub/core.cljs
+++ src/async_sandbox/pubsub/core.cljs
@@ -15,7 +15,7 @@
       (doseq [t types] (events/listen el (name t) #(put! c %)))
       c)))
 
-(defn event-msg [ev]
+(defn event->msg [ev]
   {:type (keyword (.-type ev))
    :client-x (.-clientX ev)
    :client-y (.-clientY ev)})
@@ -24,17 +24,19 @@
   (str (:client-x msg) ", " (:client-y msg)))
 
 (let [event-chan (->> (listen-chan (.-body js/document) [:mousedown :mousemove])
-                      (map< event-msg))
-      p (pub event-chan :type) ;; create a pub
-      click-chan (chan)
-      mouse-chan (chan)
+                      ;; chan an event->msg transformation
+                      (map< event->msg))
+
+      ;; create a pub
+      p (pub event-chan :type)
+
+      ;; subscribe two different channels on the same pub with two
+      ;; different topics
+      click-chan (sub p :mousedown (chan))
+      mouse-chan (sub p :mousemove (chan))
+
       click-coords (dom/getElement "click-coords")
       mouse-coords (dom/getElement "mouse-coords")]
-
-  ;; subscribe two different channels on the same pub with two
-  ;; different topics
-  (sub p :mousedown click-chan)
-  (sub p :mousemove mouse-chan)
 
   ;; two go loops feed events from two different subscriptions
   (go (while true
